d48cf91b97f3d37bd37b5960accc773f3126f9a6
Esther <argyle.stocking@gmail.com>
2022-06-10 22:23:51 -0400

    added correct javascript syntax

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -119,7 +119,7 @@ function buildAndShowHomeHTML (categories) {
       // var homeHtmlToInsertIntoMainPage = ....
 
       var homeHtmlToInsertIntoMainPage =
-        insertProperty(homeHtml, "randomCategoryShortName", chosenCategoryShortName);
+        insertProperty(homeHtml, "randomCategoryShortName", "'" + chosenCategoryShortName + "'");
 
       // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
@@ -127,6 +127,8 @@ function buildAndShowHomeHTML (categories) {
       // ....
 
       insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
+
+      console.log(homeHtmlToInsertIntoMainPage);
 
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.

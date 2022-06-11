1b43e9bb17c5adf87c2e7c2c139be07356ff74a0
Esther <argyle.stocking@gmail.com>
2022-06-10 22:37:55 -0400

    fixed javascript syntax of chosenCategoryShortName

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -103,7 +103,7 @@ function buildAndShowHomeHTML (categories) {
       // variable's name implies it expects.
       // var chosenCategoryShortName = ....
 
-      var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
+      var chosenCategoryShortName = "'" + chooseRandomCategory(categories).short_name + "'";
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
@@ -119,7 +119,7 @@ function buildAndShowHomeHTML (categories) {
       // var homeHtmlToInsertIntoMainPage = ....
 
       var homeHtmlToInsertIntoMainPage =
-        insertProperty(homeHtml, "randomCategoryShortName", "'" + chosenCategoryShortName + "'");
+        insertProperty(homeHtml, "randomCategoryShortName", chosenCategoryShortName);
 
       // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
@@ -127,8 +127,6 @@ function buildAndShowHomeHTML (categories) {
       // ....
 
       insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
-
-      console.log(homeHtmlToInsertIntoMainPage);
 
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
